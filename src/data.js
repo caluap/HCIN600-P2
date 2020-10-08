2f6183264f9a8b3f8b1a6d8299e176e7b2d230db
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2020-10-08 18:07:36 -0300

    collects data, saves it

--- src/data.js
+++ src/data.js
@@ -2,42 +2,49 @@ let soundsPath = "./assets/static/sounds";
 export var testData = {
   questions: [
     {
+      stanzaCode: "0",
       videoId: "tH6R22kdSh8",
       imageUrl: "https://i3.ytimg.com/vi/tH6R22kdSh8/hqdefault.jpg",
       correctAudioUrl: require(`${soundsPath}/correct.mp3`),
       incorrectAudioUrl: require(`${soundsPath}/incorrect.mp3`),
     },
     {
+      stanzaCode: "1",
       videoId: "G-fwuAzlBC0",
       imageUrl: "https://i3.ytimg.com/vi/G-fwuAzlBC0/hqdefault.jpg",
       correctAudioUrl: require(`${soundsPath}/correct.mp3`),
       incorrectAudioUrl: require(`${soundsPath}/incorrect.mp3`),
     },
     {
+      stanzaCode: "2",
       videoId: "krE129lP1fk",
       imageUrl: "https://i3.ytimg.com/vi/krE129lP1fk/hqdefault.jpg",
       correctAudioUrl: require(`${soundsPath}/correct.mp3`),
       incorrectAudioUrl: require(`${soundsPath}/incorrect.mp3`),
     },
     {
+      stanzaCode: "3",
       videoId: "93XpqdbMQEY",
       imageUrl: "https://i3.ytimg.com/vi/93XpqdbMQEY/hqdefault.jpg",
       correctAudioUrl: require(`${soundsPath}/correct.mp3`),
       incorrectAudioUrl: require(`${soundsPath}/incorrect.mp3`),
     },
     {
+      stanzaCode: "4",
       videoId: "53SGotIVBaY",
       imageUrl: "https://i3.ytimg.com/vi/53SGotIVBaY/hqdefault.jpg",
       correctAudioUrl: require(`${soundsPath}/correct.mp3`),
       incorrectAudioUrl: require(`${soundsPath}/incorrect.mp3`),
     },
     {
+      stanzaCode: "5",
       videoId: "-uXx33qdt-k",
       imageUrl: "https://i3.ytimg.com/vi/-uXx33qdt-k/hqdefault.jpg",
       correctAudioUrl: require(`${soundsPath}/correct.mp3`),
       incorrectAudioUrl: require(`${soundsPath}/incorrect.mp3`),
     },
     {
+      stanzaCode: "6",
       videoId: "WQ5vOWwYqts",
       imageUrl: "https://i3.ytimg.com/vi/WQ5vOWwYqts/hqdefault.jpg",
       correctAudioUrl: require(`${soundsPath}/correct.mp3`),
