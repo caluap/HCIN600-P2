cddfcf20784a08150a2393e1ce44887ed661c7d8
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2021-10-05 14:43:13 -0400

    additional fields about the participant

--- src/views/ParticipantProfile.vue
+++ src/views/ParticipantProfile.vue
@@ -14,6 +14,7 @@
           ></label
         >
         <input name="field-of-study" v-model="fieldOfStudy" />
+        <hr />
         <label for="education">Which is your current educational level?</label>
         <select name="education" v-model="selectedEducation">
           <option disabled value>Click to choose</option>
@@ -35,6 +36,176 @@
             gender
           }}</option>
         </select>
+        <hr />
+        <label for="how-important"
+          >Do you agree that climate change is an important issue?</label
+        >
+        <div class="likert-grid">
+          <label for="hi-1">
+            <input
+              type="radio"
+              id="hi-1"
+              value="1"
+              v-model="anImportantIssue"
+            />
+            Strongly disagree</label
+          >
+          <label for="hi-2">
+            <input
+              type="radio"
+              id="hi-2"
+              value="2"
+              v-model="anImportantIssue"
+            />
+            Disagree</label
+          >
+          <label for="hi-3">
+            <input
+              type="radio"
+              id="hi-3"
+              value="3"
+              v-model="anImportantIssue"
+            />
+            Neutral</label
+          >
+          <label for="hi-4">
+            <input
+              type="radio"
+              id="hi-4"
+              value="4"
+              v-model="anImportantIssue"
+            />
+            Agree</label
+          >
+          <label for="hi-5">
+            <input
+              type="radio"
+              id="hi-5"
+              value="5"
+              v-model="anImportantIssue"
+            />
+            Strongly agree</label
+          >
+        </div>
+
+        <label for="how-worried"
+          >Do you agree that climate change will have negative effects on future
+          generations?</label
+        >
+        <div class="likert-grid">
+          <label for="hw-1">
+            <input
+              type="radio"
+              id="hw-1"
+              value="1"
+              v-model="effectsOnFutureGenerations"
+            />
+            Strongly disagree</label
+          >
+          <label for="hw-2">
+            <input
+              type="radio"
+              id="hw-2"
+              value="2"
+              v-model="effectsOnFutureGenerations"
+            />
+            Disagree</label
+          >
+          <label for="hw-3">
+            <input
+              type="radio"
+              id="hw-3"
+              value="3"
+              v-model="effectsOnFutureGenerations"
+            />
+            Neutral</label
+          >
+          <label for="hw-4">
+            <input
+              type="radio"
+              id="hw-4"
+              value="4"
+              v-model="effectsOnFutureGenerations"
+            />
+            Agree</label
+          >
+          <label for="hw-5">
+            <input
+              type="radio"
+              id="hw-5"
+              value="5"
+              v-model="effectsOnFutureGenerations"
+            />
+            Strongly agree</label
+          >
+        </div>
+        <label for="what-is-the-main-cause"
+          >What do you think is the main cause of climate change?</label
+        >
+
+        <div class="radio-grid">
+          <label for="main-causes-1">
+            <input
+              type="radio"
+              id="main-causes-1"
+              value="1"
+              v-model="mainCause"
+            />
+            Greenhouse gas emissions from coal-fired power plants
+          </label>
+          <label for="main-causes-2">
+            <input
+              type="radio"
+              id="main-causes-2"
+              value="2"
+              v-model="mainCause"
+            />
+            Greenhouse gas emissions from transportation and automobiles
+          </label>
+          <label for="main-causes-3">
+            <input
+              type="radio"
+              id="main-causes-3"
+              value="3"
+              v-model="mainCause"
+            />
+            Felling of trees in primary forests to release greenhouse gases
+            stored in forests
+          </label>
+          <label for="main-causes-4">
+            <input
+              type="radio"
+              id="main-causes-4"
+              value="4"
+              v-model="mainCause"
+            />
+            Normal human activities, such as household energy use, meat
+            consumption, etc.
+          </label>
+          <label for="main-causes-5">
+            <input
+              type="radio"
+              id="main-causes-5"
+              value="5"
+              v-model="mainCause"
+            />
+            This is a natural phenomenon that happens naturally
+          </label>
+          <label for="main-causes-6">
+            <input
+              type="radio"
+              id="main-causes-6"
+              value="6"
+              v-model="mainCause"
+            />
+            Other causes not listed</label
+          >
+        </div>
+        <label for="mitigation-strategy"
+          >What do you think is the best mitigation strategy to reduce the
+          effects of climate change?
+        </label>
+        <textarea v-model="suggestedMitigationStrategies" />
       </div>
     </LongText>
     <PageNav
@@ -84,6 +255,10 @@ export default {
       selectedGender: '',
       fieldOfStudy: '',
       occupation: '',
+      anImportantIssue: null,
+      effectsOnFutureGenerations: null,
+      mainCause: null,
+      suggestedMitigationStrategies: '',
     };
   },
   methods: {
@@ -96,6 +271,10 @@ export default {
         gender: this.selectedGender,
         field_of_study: this.fieldOfStudy,
         ocupation: this.occupation,
+        is_it_an_important_issue: this.anImportantIssue,
+        will_it_affect_future_generations: this.effectsOnFutureGenerations,
+        what_is_it_main_cause: this.mainCause,
+        suggested_strategies: this.suggestedMitigationStrategies,
       });
     },
   },
@@ -106,7 +285,10 @@ export default {
       return (
         this.selectedAgeGroup != '' &&
         this.selectedEducation != '' &&
-        this.selectedGender != ''
+        this.selectedGender != '' &&
+        this.anImportantIssue != null &&
+        this.effectsOnFutureGenerations != null &&
+        this.mainCause != null
       );
     },
   },
@@ -125,8 +307,15 @@ export default {
           if (newValue.gender != null) this.selectedGender = newValue.gender;
           if (newValue.field_of_study != null)
             this.fieldOfStudy = newValue.field_of_study;
-          if (newValue.occupation != null)
-            this.occupation = newValue.occupation;
+          if (newValue.is_it_an_important_issue != null)
+            this.anImportantIssue = newValue.is_it_an_important_issue;
+          if (newValue.will_it_affect_future_generations != null)
+            this.effectsOnFutureGenerations =
+              newValue.will_it_affect_future_generations;
+          if (newValue.what_is_it_main_cause != null)
+            this.mainCause = newValue.what_is_it_main_cause;
+          if (newValue.suggested_strategies != null)
+            this.suggestedMitigationStrategies = newValue.suggested_strategies;
         }
       }
     );
@@ -143,6 +332,14 @@ export default {
         this.fieldOfStudy = this.getAboutTheParticipant.field_of_study;
       if (this.getAboutTheParticipant.occupation != null)
         this.occupation = this.getAboutTheParticipant.occupation;
+      if (this.getAboutTheParticipant.is_it_an_important_issue != null)
+        this.anImportantIssue = this.getAboutTheParticipant.is_it_an_important_issue;
+      if (this.getAboutTheParticipant.will_it_affect_future_generations != null)
+        this.effectsOnFutureGenerations = this.getAboutTheParticipant.will_it_affect_future_generations;
+      if (this.getAboutTheParticipant.what_is_it_main_cause != null)
+        this.mainCause = this.getAboutTheParticipant.what_is_it_main_cause;
+      if (this.getAboutTheParticipant.suggested_strategies != null)
+        this.suggestedMitigationStrategies = this.getAboutTheParticipant.suggested_strategies;
     }
   },
   beforeDestroy() {
@@ -154,6 +351,7 @@ export default {
 <style lang="scss" scoped>
 @import '@/assets/css/_mixins.scss';
 @import '@/assets/css/_variables.scss';
+@import '@/assets/css/_media_queries.scss';
 
 #questionnaire {
   margin-top: 2rem;
@@ -178,6 +376,49 @@ export default {
 
   border: 1px solid #aaa;
   // box-shadow: 0 1px 0 1px rgba(0, 0, 0, 0.04);
+}
+
+.radio-grid {
+  display: grid;
+  grid-gap: 1rem;
+  grid-template-columns: 1fr;
+  label {
+    display: grid;
+    align-items: center;
+    grid-template-columns: 3rem auto;
+    cursor: pointer;
+  }
+  input {
+    margin: 0;
+  }
+  margin: 1rem 0 4rem;
+}
+
+.likert-grid {
+  @extend .radio-grid;
+  @include respond-above(sm) {
+    grid-template-columns: repeat(5, 1fr);
+    text-align: center;
+    input {
+      margin: 0.5rem 0;
+    }
+    label {
+      grid-template-columns: 1fr;
+      grid-template-rows: 2rem auto;
+      align-items: flex-start;
+    }
+  }
+}
+
+textarea {
+  @extend .generic-field;
+  width: 100%;
+  font-size: 16px;
+  border: none;
+  box-sizing: border-box;
+  margin: 2rem 0 1rem;
+  padding: 1rem;
+  min-height: 12rem;
 }
 
 label {
